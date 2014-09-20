600158c0b6a8b875bdc5bbde9d3a67a945a8117c
Vidya Raghavan <vidya@shippable.com>
2014-09-20 15:59:48 +0530

    Update bowling_spec.rb

--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -5,6 +5,6 @@ describe Bowling, "#score" do
   it "returns pre for all gutter game" do
     bowling = Bowling.new
     bowling.hit
-    bowling.score.should eq("Hell World!")
+    bowling.score.should eq("Hello World!")
   end
 end
